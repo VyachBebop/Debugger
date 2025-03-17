a82bad9e486eb36466e9531d88aa7fd69481eeb1
VyachBebop <vyacheslavkolikov@gmail.com>
2025-03-17 19:16:36 +0300

    First Commit

---
+++ src/TaskThree.java
@@ -0,0 +1,16 @@
+public class TaskThree {
+    //TODO Задача 3: Нахождение ошибки в методе
+
+    public static void main(String[] args) {
+        int n = 5;
+        int result = computeFactorial(n);
+        System.out.println(n + "! = " + result);
+    }
+    public static int computeFactorial(int n) {
+        if (n == 1) { // (n == 2) Ошибка факториал должен считаться от 1 до n
+            return 1;
+        } else {
+            return n * computeFactorial(n - 1);
+        }
+    }
+}
---
+++ src/TaskTwo.java
@@ -0,0 +1,15 @@
+public class TaskTwo {
+    //TODO Задача 2: Нахождение ошибки в программе
+    // Вам предоставлена некорректная Java-программа, и ваша задача -
+    // найти и исправить ошибку с помощью отладки (debugging).
+
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3, 4, 5};
+        int sum = 0;
+
+        for (int i = 0; i <= numbers.length - 1; i++) { // Цикл заходил за границу массива
+            sum += numbers[i];
+        }
+        System.out.println("Сумма чисел: " + sum);
+    }
+}
